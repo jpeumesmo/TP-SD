170590b9d3bed0b7ee25d62e440d89173c7a864a
jpeumesmo <joaopmoferreira@gmail.com>
2017-07-10 16:25:57 -0300

    adicao do recurso do rmi para o servidor de senha

--- Servidor/src/ServidorConexao.java
+++ Servidor/src/ServidorConexao.java
@@ -1,21 +1,35 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 public class ServidorConexao implements Runnable{
 
 	//ATRIBUTOS
 
 	private Socket cliente;
-	private static ServidorSenha servidorSenha = new ServidorSenha();
+	//private static ServidorSenhaImple servidorSenha = new ServidorSenhaImple();
+	private static Senha servidorSenha;
 	private static ServidorArquivo servidorArquivo = new ServidorArquivo();
 
 	public ServidorConexao(Socket cliente){
 		this.cliente = cliente;
+		try {
+			servidorSenha = (Senha) Naming.lookup("//127.0.0.1:1099/SenhaService");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 
@@ -40,12 +54,11 @@ public class ServidorConexao implements Runnable{
 			System.out.println(cliente.getInetAddress());
 			String userName = objetoRecebido.readUTF();
 			String password = objetoRecebido.readUTF();
-			System.out.println(userName);
-			System.out.println(password);
 			boolean answer = servidorSenha.logar(userName, password);
 			objetoEnviado.writeBoolean(answer);
 			objetoEnviado.flush();
 
+				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
--- Servidor/src/ServidorSenha.java
+++ Servidor/src/ServidorSenha.java
@@ -1,48 +1,17 @@
-import java.rmi.RemoteException;
-import java.util.ArrayList;
+import java.rmi.registry.Registry;
 
-public class ServidorSenha implements Senha{
+public class ServidorSenha {
 
-	private static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
-	
 	public ServidorSenha(){
-		if (usuarios.size() == 0){
-			usuarios.add(new Usuario("root","123"));
-			usuarios.add(new Usuario("jpeumesmo","123"));
-		}
+		try{ 
+			Registry r = java.rmi.registry.LocateRegistry.createRegistry(1099);
+			r.rebind("SenhaService", new ServidorSenhaImple());
+        }  
+        catch (Exception e) { 
+            e.printStackTrace(); 
+        } 
 	}
-	@Override
-	public boolean logar(String userName,String password) throws RemoteException {
-		// TODO Auto-generated method stub
-		//TEM A ADICIONAR BD AQUI
-		for (int i = 0; i < usuarios.size(); i++) {
-			if (usuarios.get(i).getUserName().equals(userName) && 
-					usuarios.get(i).getPassword().equals(password) ){
-				return true;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public boolean useradd(String userName, String password) throws RemoteException {
-		// TODO Auto-generated method stub
-		Usuario usuario = new Usuario(userName,password);
-		usuarios.add(usuario);
-		return true;
-	}
-
-	@Override
-	public boolean userdel(String userName) throws RemoteException {
-		// TODO Auto-generated method stub
-		for (int i = 0; i < usuarios.size(); i++) {
-			if (usuarios.get(i).getUserName().equals(userName) ){
-				usuarios.remove(i);
-				return true;
-			}
-		}
-		
-		return false;
-	}
-
+	public static void main(String[] args)  { 
+        new ServidorSenha(); 
+    } 
 }
